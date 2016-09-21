5e2fc1f5c700fe58a2c2106b8c6185757b1e5ca8
R.J. LaCount <rjlacount@gmail.com>
2016-09-20 20:37:08 -0700

    Add hop / malt icons and styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,6 +7,13 @@ var context = {
   taps: []
 };
 
+Handlebars.registerHelper('times', function(n, block) {
+  var accum = '';
+  for(var i = 0; i < n; ++i)
+    accum += block.fn(i);
+  return accum;
+});
+
 function sortByTapNumber(items) {
   items.sort(function (a, b) {
     if (a.fields.tap > b.fields.tap) {
